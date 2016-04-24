4d868c45fb12dd45e74fdbc6002eca4ec22ba890
danagilliann <danalee96@gmail.com>
2016-04-24 00:14:02 -0400

    fixed sanitize issue in for a job

--- app/js/controllers/job-ctrl.js
+++ app/js/controllers/job-ctrl.js
@@ -1,13 +1,14 @@
 'use strict';
 
 angular
-.module('app.controllers')
+.module('app.controllers', ['ngSanitize'])
 .controller('JobCtrl', function($scope, $stateParams, Restangular) {
     var id = $stateParams.id;
     $scope.loadingPromise = Restangular.one('jobs/' + id)
         .get()
         .then(function(job) {
             $scope.job = job;
+            $scope.description = job.attributes.description;
         })
         .catch(function(error){
             console.log(error);
